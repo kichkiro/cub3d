29cf7bf6c886de8ff116abba1d92366c4dec7d52
kichkiro <kichkiro@student.42firenze.it>
2023-06-26 15:59:58 +0200

    miniRT init

--- project/include/minirt.h
+++ project/include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: kichkiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 00:00:19 by kichkiro          #+#    #+#             */
-/*   Updated: 2023/06/26 15:58:25 by kichkiro         ###   ########.fr       */
+/*   Updated: 2023/06/26 15:59:24 by kichkiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 
 # include "../lib/libft.h"
 # include "../lib/libmlx.h"
-# include <math.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdbool.h>
 # include <stdio.h>
+# include <math.h>
 
 // Functions ------------------------------------------------------------------>
 
