195ae44dd098a487f62551db3a59ed3fc7685c6a
kichkiro <kichkiro@student.42firenze.it>
2023-06-26 15:22:36 +0200

    init

--- project/include/minirt.h
+++ project/include/minirt.h
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kichkiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 00:00:19 by kichkiro          #+#    #+#             */
-/*   Updated: 2023/05/19 00:01:10 by kichkiro         ###   ########.fr       */
+/*   Updated: 2023/06/26 09:51:18 by kichkiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#ifndef MINIRT_H
+# define MINIRT_H
 
+// Libraries ------------------------------------------------------------------>
+
+# include "../lib/libft.h"
+# include "../lib/mlx_int.h"
+# include "../lib/mlx.h"
+# include <math.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <stdio.h>
+
+// Functions ------------------------------------------------------------------>
 
 
 #endif
