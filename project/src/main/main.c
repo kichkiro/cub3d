b8b2f5e473374fd9564cda4269c4bda34d56d08a
star-child-0 <andreavannini1@gmail.com>
2023-10-12 21:00:07 +0200

    Solved segmentation fault on t_scene_set_to_head

--- project/src/main/main.c
+++ project/src/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kichkiro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: anvannin <anvannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 23:59:30 by kichkiro          #+#    #+#             */
-/*   Updated: 2023/10/02 19:26:15 by kichkiro         ###   ########.fr       */
+/*   Updated: 2023/10/12 20:47:49 by anvannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char **argv)
 	t_scene	*scene;
 	t_mlx	*mlx;
 
+	scene = NULL;
 	validator(argc, argv);
 	parser(argv[1], &scene);
 	window_init(&mlx);
